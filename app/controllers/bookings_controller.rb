0a61af755333fde6701a3171f6e41b2a4bfee935
MarianelaMara <rodriguezmarianelamara@gmail.com>
2023-11-29 21:59:11 -0300

    cambios json api

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,44 +7,63 @@ require 'json'
         materiales_seleccionados = params["selected_provisions"].map { |string| JSON.parse(string) }
         #eligio todos los materiales que tiene que reservar
         if @materials.all? { |material| materiales_seleccionados.any? { |hash| hash["material"]["name"] == material.name.downcase } }
-        #saca el material que va a tardar mas en ser entregado
-        mayor = JSON.parse(materiales_seleccionados.max_by { |hash| hash["delivery_time"] }.to_json)
-        #saca el material que va a tardar menos en ser entregado
-        menor = JSON.parse(materiales_seleccionados.min_by { |hash| hash["delivery_time"] }.to_json)
-        start_date = Date.today + menor["delivery_time"]
-        end_date = (Date.today + mayor["delivery_time"]) + @collection.manufacturing_lead_time
-        jsonLugares = {
-            "start_date": start_date,
-            "end_date": end_date
-        }.to_json  
-        #---------------------------------------------------------------
-        BonitaApi.login
-         #seteo la variable "elegioMateriales"
-        BonitaApi.set_variable("eligioMateriales", "true", "java.lang.Boolean", @collection.id_i_bonita)
-        #seteo la variable "consultaLugares" con las fechas para que consulte los lugares de fabricacion
-        BonitaApi.set_variable("consultaLugares", "#{jsonLugares}", "java.lang.String", @collection.id_i_bonita)
-        @current_task = BonitaApi.current_task(@collection.id_i_bonita)
-        BonitaApi.assigned_task(@current_task)
-        BonitaApi.complete_task(@current_task) 
-        #----------------------------------------------------------
-        #seteo la variable "reservarMateriales" con el json para reservar en la api
-        json = {
-            "provider_type": "provider",
-            "provider_id": materiales_seleccionados[0]["provider_id"],
-            "start_date": Date.today + materiales_seleccionados[0]["delivery_time"] ,
-            "end_date": Date.today + materiales_seleccionados[0]["delivery_time"] ,
-            "quantity":@materials[0]["total_quantity"],
-            "material": materiales_seleccionados[0]["material"]["name"],
-            "delivery_place":  ""
-        }.to_json  
-        BonitaApi.set_variable("reservarMateriales", "#{json}", "java.lang.String", @collection.id_i_bonita)
-        #setear una variable con las fecha de entrega para el Timer
-        arrayFechas = [Date.today + materiales_seleccionados[0]["delivery_time"]].to_json
-        BonitaApi.set_variable("fechasTimer", "#{arrayFechas}", "java.lang.String", @collection.id_i_bonita)
-        @collection.esperar_resultados_lugares
-        redirect_to root_path , alert: "Debes elegir un lugar de fabricación para completar la reserva"
+            #saca el material que va a tardar mas en ser entregado
+            mayor = JSON.parse(materiales_seleccionados.max_by { |hash| hash["delivery_time"] }.to_json)
+            #saca el material que va a tardar menos en ser entregado
+            menor = JSON.parse(materiales_seleccionados.min_by { |hash| hash["delivery_time"] }.to_json)
+            start_date = Date.today + menor["delivery_time"]
+            end_date = (Date.today + mayor["delivery_time"]) + @collection.manufacturing_lead_time
+            jsonLugares = {
+                "start_date": start_date,
+                "end_date": end_date
+            }.to_json  
+            #---------------------------------------------------------------
+            BonitaApi.login
+            #seteo la variable "elegioMateriales"
+            BonitaApi.set_variable("eligioMateriales", "true", "java.lang.Boolean", @collection.id_i_bonita)
+            #seteo la variable "consultaLugares" con las fechas para que consulte los lugares de fabricacion
+            BonitaApi.set_variable("consultaLugares", "#{jsonLugares}", "java.lang.String", @collection.id_i_bonita)
+            @current_task = BonitaApi.current_task(@collection.id_i_bonita)
+            BonitaApi.assigned_task(@current_task)
+            BonitaApi.complete_task(@current_task) 
+            #----------------------------------------------------------
+            #seteo la variable "reservarMateriales" con el json para reservar en la api
+
+            materials = Material.find_by_sql(["SELECT materials.name, SUM(article_materials.quantity) AS total_quantity FROM materials JOIN article_materials ON materials.id = article_materials.material_id JOIN articles ON article_materials.article_id = articles.id JOIN collections ON articles.collection_id = collections.id WHERE collections.id = ? GROUP BY materials.name", params["collection_id"]])
+            json = {
+                "materials": materiales_seleccionados.map do |m|
+                    resp = @materials.find { |e| e.name.downcase == m["material"]["name"] }
+                    {
+                        "provider_type": "provider",
+                        "provider_id": m["provider_id"],
+                        "start_date": Date.today + m["delivery_time"] ,
+                        "end_date": Date.today + m["delivery_time"] ,
+                        "quantity": resp.total_quantity,
+                        "material": m["material"]["name"],
+                        "delivery_place":  ""
+                    }
+                end
+            }.to_json  
+            BonitaApi.set_variable("reservarMateriales", "#{json}", "java.lang.String", @collection.id_i_bonita)
+            #setear una variable con las fecha de entrega para el Timer
+            delivery_times = materiales_seleccionados.map { |m| Date.today + m["delivery_time"] }
+
+            fechas = {
+                "fecha": {
+                "bonita": @collection.id_i_bonita,
+                "arreglo": delivery_times
+                }
+            }.to_json 
+            BonitaApi.set_variable("fechaEntrega", "#{delivery_times.min}", "java.lang.String", @collection.id_i_bonita) 
+            BonitaApi.set_variable("fechasTimer", "#{fechas}", "java.lang.String", @collection.id_i_bonita)   
+            BonitaApi.set_variable("id", "#{@collection.id_i_bonita}", "java.lang.Integer", @collection.id_i_bonita)         
+          
+
+            @collection.esperar_resultados_lugares
+            redirect_to root_path , alert: "Debes elegir un lugar de fabricación para completar la reserva"
         else
-            puts "redefinir parametros consulta"
+            @collection.enviar_a_revision
+            redirect_to root_path , alert: "No se seleccionaron todos los materiales solicitados, se vuelve a enviar la colección a revisión para cambiar parámetros de búsqueda"
         end 
     end 
 
@@ -70,8 +89,10 @@ require 'json'
         #seteo la variable "reservarMateriales" para hacer la reserva final de los materiales pero lo hago modificando la que esta en bonita que faltaba el lugar de fabricacion de donde deben entregarse los materiales
         @reservarMateriales = BonitaApi.get_variable('reservarMateriales', @collection.id_i_bonita)
         reservarMaterialesValue = JSON.parse(@reservarMateriales["value"])
-        reservarMaterialesValue["delivery_place"] = maker["address"]
-        BonitaApi.set_variable("reservarMateriales", "#{reservarMaterialesValue}", "java.lang.String", @collection.id_i_bonita)
+        reservarMaterialesValue["materials"].map do |reserva|
+            reserva["delivery_place"] = maker["address"]
+        end
+        BonitaApi.set_variable("reservarMateriales", "#{reservarMaterialesValue.to_json}", "java.lang.String", @collection.id_i_bonita)
         #pido la tarea pendiente y la finalizo 
         @current_task = BonitaApi.current_task(@collection.id_i_bonita)
         BonitaApi.assigned_task(@current_task)
